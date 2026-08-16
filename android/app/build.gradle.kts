6a9c957d9f50bcd6e3ea32875bb452a94b2baecd
anggidwisss-boop <anggidwisss@gmail.com>
2026-08-16 22:20:16 +0800

    Bump RIMPU Android to 1.0.3

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "id.rimpu.app"
         minSdk = 23
         targetSdk = 36
-        versionCode = 3
-        versionName = "1.0.2"
+        versionCode = 4
+        versionName = "1.0.3"
     }
 
     buildTypes {
